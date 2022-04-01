9d31e36108ad9818491ce688d18b6937d298f856
yercly <95411578+yercly@users.noreply.github.com>
2022-04-01 18:00:17 +0800

    Update CoinAcc.sol

--- CoinAcc.sol
+++ CoinAcc.sol
@@ -1,38 +1,34 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity ^0.8.0;
 
 //@dev Account with mint and transfer HKD coin function
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./HKD.sol";
+contract CoinAcc is Context, IERC20, HKD {
+    event tokensMinted(address indexed owner, uint256 amount, string message);
+    event additionalTokensMinted(address indexed owner,uint256 amount,string message);
 
-contract CoinAcc {
+    mapping (address=>user)users;
+    mapping (address => uint) public balances;
 
-    address public CoinOwner;
-    address private m_tokenAddress;
-    mapping(address => uint) public customerBalance; // We create a mapping named CustomerBalance with the keys being the wallet address of our customers and value the amount of Ether they deposit in Wei.
-    //if we enter user account address , it will show their bank balance
+    struct user{        
+        address userId;
+        string  mnemonic_phrase;
+        bool isExist;
+    }
+    function userRegister(address userId,string memory mnemonic_phrase) public {
+            require(users[userId].isExist==false,"User details already registered and cannot be altered");
+            users[userId]=user(userId,mnemonic_phrase,true);
+    }
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _issue(msg.sender, 1000 * 10**decimals());
+        emit tokensMinted(msg.sender, 1000 * 10**decimals(), "Initial supply of tokens minted.");
+    }
 
-   
-    constructor(address tokenAddress) {
-        CoinOwner = msg.sender;
-        m_tokenAddress = tokenAddress; //let contract remember coin contract
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+        emit additionalTokensMinted(msg.sender, amount, "Additional tokens minted.");
     }
-    
-    function approve(uint256 intAmt) public { // set how much
-        IERC20(m_tokenAddress).approve(address(this), intAmt); //1. construct ERC20 token (HKD coin) 2. call ERC20 contract 'approve' function 3. approve function 'parameter' in this contract address
-    }
-    
-    function 
-    
-    function withdrawMoney (address payable _to, uint256 _total) public { //customer have to enter the wallet address and withdraw amount
-        require (_total <= customerBalance[msg.sender], "You have insuffient funds to withdraw");// chseck if the account has sufficient amount of money to withdraw
-        customerBalance[msg.sender] -= _total; // deduct the withdraw amount from customer's account balance
-        _to.transfer(_total); // transfer money to customer's address
-        //the transfer function is built into Solidity and transfers money to an address
-    }
-    
-    function withdrawMoney() public {
-        uint256 contractBal = IERC20(m_tokenAddress).balanceOf(address(this));
-        IERC20(m_tokenAddress).transfer(CoinOwner, contractBal);    
-    }    
+
+}
